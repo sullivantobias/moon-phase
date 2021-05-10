f56f203088b16727c12bf3e304164acefba5a45d
sullivan <sullytobias@gmail.com>
2021-05-10 17:37:50 +0200

    Release 0.1.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { loadMoonPhases } from './commons/getPhases';
 import { Navigation } from './components/navigation';
 import { Container } from './components/container';
 import { Phase } from './components/phase';
 import { PhaseLimit } from './components/phaseLimit';
 import { Separator } from './components/separator/separator';
+import { Calendar } from './components/calendar';
+import { Loader } from './components/loader';
 
 import './commons/styles/app.scss';
 import './commons/styles/body.scss';
-import { Calendar } from './components/calendar';
+
 
 
 export const App = () => {
@@ -16,9 +18,14 @@ export const App = () => {
   const [phaseLimit, setPhaseLimit] = useState([])
   const [phases, setPhases] = useState([])
   const [currentMonth, setCurrentMonth] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const appRef = useRef()
 
   useEffect(() => {
-    getDatas()
+    setTimeout(() => {
+      getDatas()
+    }, 3000)
   }, [])
 
   const getDatas = async () => {
@@ -28,6 +35,10 @@ export const App = () => {
       const day = new Date().getDate();
       setCurrentMonth(`${day} ${datas.monthName}`)
       getAllPhases(datas.phase, day)
+
+      setLoading(false);
+
+      appRef.current.classList.add('loaded');
     }
   }
 
@@ -43,22 +54,29 @@ export const App = () => {
 
   return (
 
-    <div className="App">
-      <Navigation links={[
-        { anchor: 'today', label: 'Today' },
-        { anchor: 'keyPhase', label: 'Key Phases' },
-        { anchor: 'calendar', label: 'Calendar' }]} />
-      <Container anchor='today' title={currentMonth}>
-        <Phase phaseSvg={currentDayPhase.svg} />
-      </Container>
-      <Separator />
-      <Container anchor='keyPhase' title='Key Phases'>
-        <PhaseLimit phaseLimit={phaseLimit} />
-      </Container>
-      <Separator />
-      <Container anchor='calendar' title='Calendar'>
-        <Calendar phases={phases} />
-      </Container>
+    <div className="App" >
+      <div ref={appRef} className='App__wrapper'>
+        {!loading &&
+          <>
+            <Navigation links={[
+              { anchor: 'today', label: 'Today' },
+              { anchor: 'keyPhase', label: 'Key Phases' },
+              { anchor: 'calendar', label: 'Calendar' }]} />
+
+            <Container anchor='today' title={currentMonth}>
+              <Phase phaseSvg={currentDayPhase.svg} />
+            </Container>
+            <Separator />
+            <Container anchor='keyPhase' title='Key Phases'>
+              <PhaseLimit phaseLimit={phaseLimit} />
+            </Container>
+            <Separator />
+            <Container anchor='calendar' title='Calendar'>
+              <Calendar phases={phases} />
+            </Container>
+          </>}
+      </div>
+      { loading && <Loader />}
     </div>
   );
 }
